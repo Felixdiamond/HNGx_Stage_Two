38635c0065f602145aee82dab6942dc6546659e5
Felixdiamond <diamondfelix006@gmail.com>
2023-09-12 23:42:42 +0100

    update code

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 const connectDb = require("./config/dbConnection");
 
 dotenv.config();
 
-connectDb();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the "static" directory
-app.use(express.static('static'));
+app.use(express.static("static"));
 
 app.use(bodyParser.json());
 
 app.use("/api", require("./routes/personRouter"));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+connectDb().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 });
