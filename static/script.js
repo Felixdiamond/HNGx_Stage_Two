4c99620be3ad3175e50cc9df6a5961d6e2134ecc
Felixdiamond <diamondfelix006@gmail.com>
2023-09-12 23:55:53 +0100

    update code

--- static/script.js
+++ static/script.js
@@ -1,22 +1,44 @@
-document.getElementById('personForm').addEventListener('submit', function(event) {
+document
+  .getElementById("personForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const age = document.getElementById('age').value;
-
-    fetch('http://localhost:4000/api', {
-        method: 'POST',
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    if (document.getElementById("email").value == null || "") {
+      fetch("http://localhost:4000/api", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ name, age }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('message').textContent = 'Account has been created successfully!';
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          document.getElementById("message").textContent =
+            "Account has been created successfully!";
         })
         .catch((error) => {
-            console.error('Error:', error);
-            document.getElementById('message').textContent = 'An error occurred.';
+          console.error("Error:", error);
+          document.getElementById("message").textContent = "An error occurred.";
         });
-});
+    } else {
+      const email = document.getElementById("email").value;
+      fetch("http://localhost:4000/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, age, email }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          document.getElementById("message").textContent =
+            "Account has been created successfully!";
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          document.getElementById("message").textContent = "An error occurred.";
+        });
+    }
+  });
